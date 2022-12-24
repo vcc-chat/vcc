67f2b58598d1030a1b72af30d9d7c0aab3cd67dd
3swordman <yyuxiaoran@qq.com>
2022-12-24 13:58:34 +0800

    feat: use databases for auth

--- services/login.py
+++ services/login.py
@@ -2,15 +2,35 @@
 
 import base
 
+from peewee import *
+
+db = SqliteDatabase("users.db") # just for test
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    name = CharField(max_length=16, unique=True)
+    password = CharField(max_length=16)
+
 class Main:
     def login(self, username, password):
-        users = {
-            "test1": "passwd1",
-            "test2": "passwd2",
-        }
-        return username in users and users[username] == password
+        try:
+            User.get(User.name == username, User.password == password)
+            return True
+        except:
+            return False
+    
+    def register(self, username, password):
+        try:
+            User.create(name=username, password=password)
+            return True
+        except:
+            return False
 
 if __name__ == "__main__":
+    db.create_tables([User])
     server = base.RpcServer()
     server.register(Main())
     server.connect()

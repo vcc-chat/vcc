86e643aea529e9f160762ba51b973b699a202e4c
calvin <calvinchaochao@outlook.com>
2022-12-27 15:45:46 +0800

    AAA

--- vcc_lib/__init__.py
+++ vcc_lib/__init__.py
@@ -35,7 +35,8 @@ class RpcExchangerRpcHandler:
                 result = await self._exchanger.rpc_request(provider+"/"+service, data)
                 log.debug(f"{result=}")
                 return result
-        return type(provider,(),{"__getattr__":lambda self,service:wrapper})()
+            return func
+        return type(provider,(),{"__getattr__":lambda self,service:wrapper()})()
 
 class RpcExchanger:
     """Low-level api which is hard to use"""

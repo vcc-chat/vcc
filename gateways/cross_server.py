bff9a1bf2bca922c62907eebfc50d2b1247a1088
3swordman <yyuxiaoran@qq.com>
2023-03-18 11:08:50 +0800

    feat: improve cross server gateway

--- gateways/cross_server.py
+++ gateways/cross_server.py
@@ -2,53 +2,55 @@ import asyncio
 import json
 import os
 import weakref
+import functools
 
-from websockets.server import serve, broadcast
+from websockets import serve, broadcast
 
 import vcc.vcc as vcc
 
-allow_list = os.getenv("VCC_SAFE_SERVERS", "").split(",")
+allow_list = os.getenv("VCC_SAFE_SERVERS", "127.0.0.1,::1").split(",")
+
+async def websocket_send(websocket, id, data=None):
+    await websocket.send(json.dumps({
+        "data": data,
+        "id": id
+    }))
+
+websocket_recv_list = weakref.WeakSet()
 
 async def handle(text, websocket, exchanger: vcc.RpcExchanger):
     data = json.loads(text)
     service = data["service"]
+    send = functools.partial(websocket_send, websocket, data["id"])
     if service == "message/send_message":
         await exchanger._redis.publish("messages", json.dumps(data["data"]))
-        await websocket.send(json.dumps({
-            "data": None,
-            "id": data["id"]
-        }))
+        await send(None)
     elif service == "message/send_event":
         await exchanger._redis.publish("events", json.dumps(data["data"]))
-        await websocket.send(json.dumps({
-            "data": None,
-            "id": data["id"]
-        }))
+        await send(None)
+    elif service == "message/start_recv":
+        websocket_recv_list.add(websocket)
+        await send(None)
     else:
-        await websocket.send(json.dumps({
-            "data": await exchanger.rpc_request(data["service"], data["data"]),
-            "id": data["id"]
-        }))
-
-websocket_list = weakref.WeakSet()
+        await send(await exchanger.rpc_request(data["service"], data["data"]))
 
 async def handler(exchanger: vcc.RpcExchanger, websocket):
-    if websocket.remote_address[0] not in websocket:
+    if websocket.remote_address[0] not in allow_list:
         await websocket.close(1008, "Disallowed server")
         return
+        
     task_list: set[asyncio.Task] = set()
     try:
-        websocket_list.add(websocket)
         async for i in websocket:
-            task_list.add(handle(i, websocket, exchanger))
+            task_list.add(asyncio.create_task(handle(i, websocket, exchanger)))
     finally:
-        websocket_list.discard(websocket)
+        websocket_recv_list.discard(websocket)
         for task in task_list:
             task.cancel()
 
 async def broadcast_task(exchanger: vcc.RpcExchanger):
     async for rpc_message in exchanger.pubsub.listen():
-        broadcast(websocket_list, rpc_message["data"])
+        broadcast(websocket_recv_list, rpc_message["data"].decode())
 
 async def main():
     async with vcc.RpcExchanger() as exchanger:
